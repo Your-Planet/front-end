ed62be2e02184c889acfd1c0b6287cedaba38324
hyeonQyu <dhk0561@naver.com>
2023-11-20 01:14:31 +0900

    refactor: 컨벤션 맞추어 수정

--- src/components/register/author/RegisterAuthorFormView.tsx
+++ src/components/register/author/RegisterAuthorFormView.tsx
@@ -44,49 +44,49 @@ function RegisterAuthorFormView(props: RegisterAuthorFormViewProps) {
 
 	return (
 		<>
-			<div className={"max-w-[520px] mx-auto py-28"}>
+			<div className="max-w-[520px] mx-auto py-28">
 				<H2>회원 가입 (작가)</H2>
 
 				<FormProvider {...form}>
-					<form onSubmit={handleFormSubmit} className={"mt-8 flex flex-col gap-4"}>
+					<form onSubmit={handleFormSubmit} className="mt-8 flex flex-col gap-4">
 						<TextField
-							formName={"email"}
-							label={"이메일"}
+							formName="email"
+							label="이메일"
 							required
 							rules={{
 								...getEmailValidateRule(),
 							}}
-							placeholder={"abc12@naver.com"}
+							placeholder="abc12@naver.com"
 							fullWidth
 						/>
 
 						{/*TODO @김현규 비밀번호 조건*/}
 						<TextField
-							formName={"password"}
-							label={"비밀번호"}
+							formName="password"
+							label="비밀번호"
 							required
-							placeholder={"TODO @김현규 비밀번호 조건"}
-							type={"password"}
+							placeholder="TODO @김현규 비밀번호 조건"
+							type="password"
 							fullWidth
 						/>
 
 						<TextField
-							formName={"passwordConfirm"}
-							label={"비밀번호 확인"}
+							formName="passwordConfirm"
+							label="비밀번호 확인"
 							required
 							rules={{
 								...getPasswordConfirmValidateRule(watchedValue.password!),
 							}}
-							placeholder={"비밀번호를 다시 한번 입력하세요"}
-							type={"password"}
+							placeholder="비밀번호를 다시 한번 입력하세요"
+							type="password"
 							fullWidth
 						/>
 
-						<TextField formName={"name"} label={"이름"} required fullWidth />
+						<TextField formName="name" label="이름" required fullWidth />
 
 						<RadioGroup<SexType>
-							label={"성별"}
-							formName={"sex"}
+							label="성별"
+							formName="sex"
 							required
 							radios={[
 								{
@@ -101,20 +101,20 @@ function RegisterAuthorFormView(props: RegisterAuthorFormViewProps) {
 						/>
 
 						<TextField
-							formName={"tel"}
-							label={"연락처"}
+							formName="tel"
+							label="연락처"
 							required
 							validator={isNumber}
-							placeholder={"숫자만 입력하세요"}
+							placeholder="숫자만 입력하세요"
 							fullWidth
-							type={"tel"}
+							type="tel"
 						/>
 
 						{/*TODO @김현규 생년월일*/}
 
-						<TextField formName={"instagramId"} label={"인스타그램 아이디"} required fullWidth />
+						<TextField formName="instagramId" label="인스타그램 아이디" required fullWidth />
 
-						<Button type={"submit"} variant={"contained"} size={"large"} fullWidth>
+						<Button type="submit" variant="contained" size="large" fullWidth>
 							가입하기
 						</Button>
 					</form>
