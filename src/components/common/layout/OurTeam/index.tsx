1be69246adb17b110005abdf3c008b4a1aebd77f
naeunchan <eunchan0492@naver.com>
2023-11-20 22:46:28 +0900

    feat: images and typography

--- src/components/common/layout/OurTeam/index.tsx
+++ src/components/common/layout/OurTeam/index.tsx
@@ -2,33 +2,69 @@ import { Box, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import marketing from "@/public/images/marketing.png";
+import productDesign from "@/public/images/product-design.png";
 import branding from "@/public/images/branding.png";
 import dataAnalysis from "@/public/images/data-analysis.png";
-import productDesign from "@/public/images/product-design.png";
 
 const OurTeam = () => {
 	return (
 		<Box className="w-full h-screen flex justify-center items-center">
-			<Box className="w-1/2 h-full flex flex-col justify-center items-center mx-0 my-auto">
-				<Typography className="" variant="h4">
+			<Box className="w-3/5 h-full flex flex-col justify-center">
+				<Typography className="p-3" variant="h4">
 					Our Team
 				</Typography>
 				<Box className="w-full h-1/2 flex justify-center items-center">
-					<Box className="flex relative w-1/2 h-1/2">
-						<Image src={marketing} alt="marketing" fill />
-						<Typography variant="subtitle2">마케팅</Typography>
+					<Box className="w-full h-full flex flex-col justify-center p-3">
+						<Box className="flex relative w-full h-1/2">
+							<Image
+								src={marketing}
+								alt="marketing"
+								fill
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</Box>
+						<Typography className="text-center mt-3" variant="h5">
+							마케팅
+						</Typography>
 					</Box>
-					<Box className="flex relative w-1/2 h-1/2">
-						<Image src={productDesign} alt="product-design" fill />
-						<Typography variant="subtitle2">기획</Typography>
+					<Box className="w-full h-full flex flex-col justify-center p-3">
+						<Box className="flex relative w-full h-1/2">
+							<Image
+								src={productDesign}
+								alt="product design"
+								fill
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</Box>
+						<Typography className="text-center mt-3" variant="h5">
+							기획
+						</Typography>
 					</Box>
-					<Box className="flex relative w-1/2 h-1/2">
-						<Image src={branding} alt="branding" fill />
-						<Typography variant="subtitle2">브랜딩</Typography>
+					<Box className="w-full h-full flex flex-col justify-center p-3">
+						<Box className="flex relative w-full h-1/2">
+							<Image
+								src={branding}
+								alt="branding"
+								fill
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</Box>
+						<Typography className="text-center mt-3" variant="h5">
+							브랜딩
+						</Typography>
 					</Box>
-					<Box className="flex relative w-1/2 h-1/2">
-						<Image src={dataAnalysis} alt="data-analysis" fill />
-						<Typography variant="subtitle2">데이터 분석</Typography>
+					<Box className="w-full h-full flex flex-col justify-center p-3">
+						<Box className="flex relative w-full h-1/2">
+							<Image
+								src={dataAnalysis}
+								alt="data analysis"
+								fill
+								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</Box>
+						<Typography className="text-center mt-3" variant="h5">
+							데이터 분석
+						</Typography>
 					</Box>
 				</Box>
 			</Box>
