2faa3a230dfebbdda066a4a2135be2f242321948
naeunchan <eunchan0492@naver.com>
2023-11-20 23:28:58 +0900

    image size 조절

--- src/components/common/layout/OurTeam/index.tsx
+++ src/components/common/layout/OurTeam/index.tsx
@@ -9,58 +9,38 @@ import dataAnalysis from "@/public/images/data-analysis.png";
 const OurTeam = () => {
 	return (
 		<Box className="w-full h-screen flex justify-center items-center">
-			<Box className="w-3/5 h-full flex flex-col justify-center">
+			<Box className="w-full h-full max-w-[1200px] flex flex-col justify-center ml-40 mr-40">
 				<Typography className="p-3" variant="h4">
 					Our Team
 				</Typography>
-				<Box className="w-full h-1/2 flex justify-center items-center">
-					<Box className="w-full h-full flex flex-col justify-center p-3">
-						<Box className="flex relative w-full h-1/2">
-							<Image
-								src={marketing}
-								alt="marketing"
-								fill
-								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-							/>
+				<Box className="w-full flex justify-center items-center pt-20">
+					<Box className="w-full h-full flex flex-col items-center">
+						<Box className="flex relative w-[220px] h-[220px]">
+							<Image src={marketing} alt="marketing" fill priority={true} sizes="(max-width: 768px) 200px, 220px" />
 						</Box>
 						<Typography className="text-center mt-3" variant="h5">
 							마케팅
 						</Typography>
 					</Box>
-					<Box className="w-full h-full flex flex-col justify-center p-3">
-						<Box className="flex relative w-full h-1/2">
-							<Image
-								src={productDesign}
-								alt="product design"
-								fill
-								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-							/>
+					<Box className="w-full h-full flex flex-col items-center">
+						<Box className="flex relative w-[220px] h-[220px]">
+							<Image src={productDesign} alt="product design" fill sizes="(max-width: 768px) 200px, 220px" />
 						</Box>
 						<Typography className="text-center mt-3" variant="h5">
 							기획
 						</Typography>
 					</Box>
-					<Box className="w-full h-full flex flex-col justify-center p-3">
-						<Box className="flex relative w-full h-1/2">
-							<Image
-								src={branding}
-								alt="branding"
-								fill
-								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-							/>
+					<Box className="w-full h-full flex flex-col items-center">
+						<Box className="flex relative w-[220px] h-[220px]">
+							<Image src={branding} alt="branding" fill sizes="(max-width: 768px) 200px, 220px" />
 						</Box>
 						<Typography className="text-center mt-3" variant="h5">
 							브랜딩
 						</Typography>
 					</Box>
-					<Box className="w-full h-full flex flex-col justify-center p-3">
-						<Box className="flex relative w-full h-1/2">
-							<Image
-								src={dataAnalysis}
-								alt="data analysis"
-								fill
-								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-							/>
+					<Box className="w-full h-full flex flex-col items-center">
+						<Box className="flex relative w-[220px] h-[220px]">
+							<Image src={dataAnalysis} alt="data analysis" fill sizes="(max-width: 768px) 200px, 220px" />
 						</Box>
 						<Typography className="text-center mt-3" variant="h5">
 							데이터 분석
