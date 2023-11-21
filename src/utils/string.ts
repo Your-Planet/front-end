c01873307f42837e86642907320a707c43979081
khg0408 <khg0408@midasin.com>
2023-11-21 15:38:55 +0900

    etc: 기타 수정

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -8,6 +8,4 @@ export const isNumber = (str: string): boolean => /^[0-9]*$/.test(str);
  * 이메일 여부 판단
  * @param str
  */
-export const isEmail = (str: string): boolean => {
-	return /^[A-Za-z0-9_.\-]{1,64}@[A-Za-z0-9_.\-]{1,260}$/.test(str);
-};
+export const isEmail = (str: string): boolean => /^[A-Za-z0-9_.\-]{1,64}@[A-Za-z0-9_.\-]{1,260}$/.test(str);
