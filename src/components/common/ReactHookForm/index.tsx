a091f4f75ad1d848145aac5523ed14f4c1f727e7
hyeonQyu <dhk0561@naver.com>
2023-11-22 15:01:37 +0900

    feat: react-hook-form DatePicker 컴포넌트 추가

--- src/components/common/ReactHookForm/index.tsx
+++ src/components/common/ReactHookForm/index.tsx
@@ -6,17 +6,22 @@ import ReactHookFormTextField, {
 import ReactHookFormRadioGroup, {
 	ReactHookFormRadioGroupProps,
 } from "@/components/common/ReactHookForm/components/RadioGroup";
+import ReactHookFormDatePicker, {
+	ReactHookFormDatePickerProps,
+} from "@/components/common/ReactHookForm/components/DatePicker";
 
 const ReactHookForm = <TFieldValues extends FieldValues = FieldValues>(): {
 	TextField: JSXElementConstructor<ReactHookFormTextFieldProps<TFieldValues>>;
 	RadioGroup: <RadioValue extends string | number>(
 		props: ReactHookFormRadioGroupProps<TFieldValues, RadioValue>,
 	) => ReactElement<ReactHookFormRadioGroupProps<TFieldValues, RadioValue>>;
+	DatePicker: JSXElementConstructor<ReactHookFormDatePickerProps<TFieldValues>>;
 } => {
 	return {
 		TextField: ReactHookFormTextField<TFieldValues>,
 		RadioGroup: <RadioValue extends string | number>(props: ReactHookFormRadioGroupProps<TFieldValues, RadioValue>) =>
 			ReactHookFormRadioGroup<TFieldValues, RadioValue>(props),
+		DatePicker: ReactHookFormDatePicker<TFieldValues>,
 	};
 };
 
