d099f44b2dbca9eb3209543853ed9974b33e9b6f
hyeonQyu <dhk0561@naver.com>
2023-11-22 21:39:41 +0900

    feat: 광고주 회원가입 페이지 추가

---
+++ src/app/register/advertiser/page.tsx
@@ -0,0 +1,11 @@
+import RegisterAdvertiserFormView from "@/components/register/advertiser/RegisterAdvertiserFormView";
+
+export interface RegisterAdvertiserPageProps {}
+
+function RegisterAdvertiserPage(props: RegisterAdvertiserPageProps) {
+	const {} = props;
+
+	return <RegisterAdvertiserFormView />;
+}
+
+export default RegisterAdvertiserPage;
---
+++ src/components/register/advertiser/RegisterAdvertiserFormView.tsx
@@ -0,0 +1,13 @@
+export interface RegisterAdvertiserFormViewProps {}
+
+function RegisterAdvertiserFormView(props: RegisterAdvertiserFormViewProps) {
+	const {} = props;
+
+	return (
+		<>
+			<div></div>
+		</>
+	);
+}
+
+export default RegisterAdvertiserFormView;
