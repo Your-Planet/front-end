8b8566ac2dd7d392468be9248142a8b5c7c60e65
hyeonQyu <dhk0561@naver.com>
2023-11-28 08:59:14 +0900

    fix: 누락 숫자 추가

--- src/components/common/password/PasswordTextField/components/PasswordValidationMessages/index.tsx
+++ src/components/common/password/PasswordTextField/components/PasswordValidationMessages/index.tsx
@@ -8,7 +8,7 @@ import {
 	checkValidSequenceContinuity,
 } from "@/utils/password";
 import classNames from "classnames";
-import { PASSWORD_LENGTH } from "@/defines/password/constants";
+import { COMBINED_MIN_LIMIT, CONTINUOUS_DIGITS_LIMIT, PASSWORD_LENGTH } from "@/defines/password/constants";
 
 export interface IndexProps {
 	password: string;
@@ -26,15 +26,15 @@ function PasswordValidationMessages(props: IndexProps) {
 			valid: checkValidLength(password, PASSWORD_LENGTH.min, PASSWORD_LENGTH.max),
 		},
 		{
-			message: "영어 대문자, 소문자, 숫자, 특수문자 중 3종류 문자 조합으로 구성",
+			message: `영어 대문자, 소문자, 숫자, 특수문자 중 ${COMBINED_MIN_LIMIT}종류 문자 조합으로 구성`,
 			valid: checkValidCombination(password),
 		},
 		{
-			message: "동일한 문자 3자리 연속 사용 금지",
+			message: `동일한 문자 ${CONTINUOUS_DIGITS_LIMIT}자리 연속 사용 금지`,
 			valid: checkValidSameCharContinuity(password),
 		},
 		{
-			message: "자리 이상 연속적이거나 키보드상 연속된 문자 및 숫자열 제한",
+			message: `${CONTINUOUS_DIGITS_LIMIT}자리 이상 연속적이거나 키보드상 연속된 문자 및 숫자열 제한`,
 			valid: checkValidSequenceContinuity(password) && checkValidKeyboardContinuity(password),
 		},
 	];
