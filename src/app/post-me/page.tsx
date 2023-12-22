60a220cbcab9ba2b626291110ff2e1b098f9718b
naeunchan <eunchan0492@naver.com>
2023-12-22 22:59:35 +0900

    feat: post me page
    instagram account
    introduction
    instatoon category

--- src/app/post-me/page.tsx
+++ src/app/post-me/page.tsx
@@ -1,9 +1,19 @@
+import InstagramIdField from "@/components/post_me/instagramIdField";
+import InstatoonCategory from "@/components/post_me/InstatoonCategory";
+import IntroductionField from "@/components/post_me/IntroductionField";
+import { Box } from "@mui/system";
 import React from "react";
 
 type Props = {};
 
 const PostME = (props: Props) => {
-	return <div className="w-full h-screen">PostME</div>;
+	return (
+		<Box className="w-full flex flex-col justify-center items-center p-10">
+			<InstagramIdField />
+			<IntroductionField />
+			<InstatoonCategory />
+		</Box>
+	);
 };
 
 export default PostME;
