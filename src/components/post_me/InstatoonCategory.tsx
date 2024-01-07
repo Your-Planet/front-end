b192a7d3b0627dd5e8a6b800b7f8303ebf9ff584
naeunchan <eunchan0492@naver.com>
2024-01-07 14:50:43 +0700

    chore: TODO 추가

--- src/components/post_me/InstatoonCategory.tsx
+++ src/components/post_me/InstatoonCategory.tsx
@@ -1,16 +1,27 @@
-import { Box, Checkbox, FormControlLabel, FormGroup, Typography } from "@mui/material";
+import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel } from "@mui/material";
 
 const InstatoonCategory = () => {
+	// TODO: search branch 머지 후 카테고리 선택 기능 수정
 	return (
 		<Box className="flex flex-col w-[50vw] pt-3">
-			<Typography variant="subtitle1">인스타툰 카테고리 (최대 3개)</Typography>
-			<FormGroup className="pt-1">
-				<FormControlLabel className="w-fit" label="일상툰" control={<Checkbox size="small" />} />
-				<FormControlLabel className="w-fit" label="유머툰" control={<Checkbox size="small" />} />
-				<FormControlLabel className="w-fit" label="공감툰" control={<Checkbox size="small" />} />
-				<FormControlLabel className="w-fit" label="연애툰" control={<Checkbox size="small" />} />
-				<FormControlLabel className="w-fit" label="에세이툰" control={<Checkbox size="small" />} />
-			</FormGroup>
+			<FormControl
+				required
+				// error={error}
+				component="fieldset"
+				sx={{ m: 3 }}
+				variant="standard"
+			>
+				<FormLabel component="legend">인스타툰 카테고리 (최대 3개)</FormLabel>
+				<FormGroup className="pt-1">
+					{/* TODO: search 브랜치 머지 후 label 체크박스 수정*/}
+					<FormControlLabel className="w-fit" label="일상툰" control={<Checkbox size="small" />} />
+					<FormControlLabel className="w-fit" label="유머툰" control={<Checkbox size="small" />} />
+					<FormControlLabel className="w-fit" label="공감툰" control={<Checkbox size="small" />} />
+					<FormControlLabel className="w-fit" label="연애툰" control={<Checkbox size="small" />} />
+					<FormControlLabel className="w-fit" label="에세이툰" control={<Checkbox size="small" />} />
+				</FormGroup>
+				<FormHelperText>최소 1개를 선택해 주세요</FormHelperText>
+			</FormControl>
 		</Box>
 	);
 };
