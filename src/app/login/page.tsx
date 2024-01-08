4a25fd6179eb9cb2ee853373160c9cea5e328b08
hyeonQyu <dhk0561@naver.com>
2024-01-08 22:17:09 +0900

    feat: 로그인 페이지 추가

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+export interface LoginPageProps {}
+
+function LoginPage(props: LoginPageProps) {
+	const {} = props;
+
+	return (
+		<>
+			<div></div>
+
+			<style jsx>{``}</style>
+		</>
+	);
+}
+
+export default LoginPage;
