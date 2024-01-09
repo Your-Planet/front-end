1dc9f0a3879a6c341de82af4b1397b9ebfc62c0e
hyeonQyu <dhk0561@naver.com>
2024-01-09 23:52:16 +0900

    feat: 로그인 UI 완료 및 유효성 검사 로직 추가

--- src/components/login/LoginView/index.tsx
+++ src/components/login/LoginView/index.tsx
@@ -2,6 +2,8 @@ import Logo from "@/components/common/Logo";
 import { FormProvider, useForm } from "react-hook-form";
 import { LoginForm } from "@/components/login/LoginView/defines/types";
 import ReactHookForm from "@/components/common/ReactHookForm";
+import { Button } from "@mui/material";
+import { isEmail } from "@/utils/string";
 
 export interface LoginViewProps {}
 
@@ -21,17 +23,51 @@ function LoginView(props: LoginViewProps) {
 		console.log(data);
 	});
 
+	const validateEmail = (value: string) => {
+		if (!value) return "이메일을 입력해 주세요.";
+		if (!isEmail(value)) return "이메일 형식에 맞게 입력해 주세요.";
+		return true;
+	};
+
+	const validatePassword = (value: string) => {
+		if (!value) return "비밀번호를 입력해 주세요.";
+		return true;
+	};
+
 	const { TextField } = ReactHookForm<LoginForm>();
 
 	return (
 		<>
-			<div className="m-auto flex flex-col gap-24">
-				<Logo />
+			<div className="m-auto flex flex-col gap-12 p-8">
+				<div className="text-center">
+					<Logo />
+				</div>
 
 				<FormProvider {...form}>
-					<form>
-						<TextField formName="email" label="이메일" fullWidth />
-						<TextField formName="password" label="비밀번호" type="password" fullWidth />
+					<form onSubmit={handleFormSubmit}>
+						<TextField
+							formName="email"
+							label="이메일"
+							type="email"
+							fullWidth
+							margin="normal"
+							rules={{
+								validate: validateEmail,
+							}}
+						/>
+						<TextField
+							formName="password"
+							label="비밀번호"
+							type="password"
+							fullWidth
+							margin="normal"
+							rules={{
+								validate: validatePassword,
+							}}
+						/>
+						<Button fullWidth variant="contained" size="large" type="submit">
+							로그인
+						</Button>
 					</form>
 				</FormProvider>
 			</div>
