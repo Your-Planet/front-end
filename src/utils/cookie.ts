1c927f17adcd23aefba77196f646e0234f061d8b
hyeonQyu <dhk0561@naver.com>
2024-01-11 22:44:40 +0900

    feat: 쿠키 설정 관련 유틸 추가

---
+++ src/utils/cookie.ts
@@ -0,0 +1,30 @@
+import { getCookie as readCookie, setCookie as saveCookie, deleteCookie } from "cookies-next";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { DefaultOptions } from "cookies-next/lib/types";
+
+dayjs.extend(utc);
+
+type CookieOptions = Omit<DefaultOptions, "expires">;
+
+export const getRootDomain = (url: string) => url.split(".").slice(-2).join(".");
+
+export const getCookie = (name: string) => readCookie(name) as any;
+
+export const setCookie = (name: string, value: string, atExpires?: number, options?: CookieOptions) => {
+	const atExpiresDate = atExpires ? dayjs().utc().add(atExpires, "s").toDate() : undefined;
+
+	saveCookie(name, value, {
+		...options,
+		path: "/",
+		expires: atExpiresDate,
+		domain: options?.domain ?? getRootDomain(globalThis?.location?.hostname ?? options?.req?.headers?.host ?? ""),
+	});
+};
+
+export const removeCookie = (name: string) => {
+	deleteCookie(name, {
+		path: "/",
+		domain: getRootDomain(window.location.hostname),
+	});
+};
