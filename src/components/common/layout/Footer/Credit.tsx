c6ea06c7512c76f0ee265443f3770e196ef864a1
naeunchan <eunchan0492@naver.com>
2024-01-16 16:51:58 +0700

    chore: add unstaged files

---
+++ src/components/common/layout/Footer/Credit.tsx
@@ -0,0 +1,45 @@
+type Props = {};
+
+function Credit({}: Props) {
+	return (
+		<div>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="어려움 아이콘">
+				어려움 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="협동 아이콘">
+				협동 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="마케팅 아이콘">
+				마케팅 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="단계 아이콘">
+				단계 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="여자 아이콘">
+				여자 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/-" title="손과 몸짓 아이콘">
+				손과 몸짓 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/-" title="- 아이콘">
+				- 아이콘 제작자: Konkapp - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title="모서리 아이콘">
+				모서리 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title=" 아이콘">
+				{" "}
+				아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/ui" title="ui 아이콘">
+				Ui 아이콘 제작자: Freepik - Flaticon
+			</a>
+			<a href="https://www.flaticon.com/kr/free-icons/" title=" 아이콘">
+				{" "}
+				아이콘 제작자: Freepik - Flaticon
+			</a>
+		</div>
+	);
+}
+
+export default Credit;
