3eaaa821212717a5a5399316f0022e8073876d4c
naeunchan <eunchan0492@naver.com>
2024-01-16 17:21:48 +0700

    refactor: refactoring our_team

--- src/components/common/layout/OurTeam/index.tsx
+++ src/components/common/layout/OurTeam/index.tsx
@@ -4,12 +4,10 @@ import Card from "./Card";
 
 function OurTeam() {
 	return (
-		<Box className="w-full h-auto relative">
-			<Box className="px-[100px] h-auto">
-				<Box className="flex items-center justify-center w-full h-except-header">
-					<H4Bold text="직접 경험해보세요" />
-				</Box>
-				<Box className="flex overflow-hidden bg-white px-[60px] py-0 whitespace-nowrap relative before:absolute before:top-0 before:w-[300px] before:h-full before:content-none before:z-10 before:left-0 after:absolute after:top-0 after:w-[300px] after:h-full after:content-none after:z-10 hover:pause mb-3">
+		<Box className="w-full h-auto px-[100px] relative">
+			<Box className="flex flex-col items-center justify-center w-full h-except-header">
+				<H4Bold text="직접 경험해보세요" />
+				<Box className="flex overflow-hidden bg-white px-[60px] mt-12 whitespace-nowrap relative before:absolute before:top-0 before:w-[300px] before:h-full before:content-none before:z-10 before:left-0 after:absolute after:top-0 after:w-[300px] after:h-full after:content-none after:z-10 hover:pause">
 					<Box className="flex hover:pause animate-slide">
 						<Card text="마켓팅" backgroundColor="bg-indigo-300" imageSrc="/images/marketing.png" imageAlt="marketing" />
 						<Card text="거래" backgroundColor="bg-yellow-300" imageSrc="/images/business.png" imageAlt="business" />
