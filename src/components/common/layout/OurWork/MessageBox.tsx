4dd12e1da6ecb92409814d805d93109a3f427a28
naeunchan <eunchan0492@naver.com>
2024-01-21 22:14:29 +0700

    build: fix build error

--- src/components/common/layout/OurWork/MessageBox.tsx
+++ src/components/common/layout/OurWork/MessageBox.tsx
@@ -12,19 +12,22 @@ type Props = {
 function MessageBox(props: Props) {
 	const { children, emoticon, className, direction = "chat-start" } = props;
 
+	const renderAvatar =
+		direction === "chat-start" ? (
+			<Box className="avatar mr-5">
+				<Typography variant="h2">{emoticon}</Typography>
+			</Box>
+		) : (
+			<Box className="avatar ml-5">
+				<Typography variant="h2">{emoticon}</Typography>
+			</Box>
+		);
+
 	return (
 		<Box className={`chat items-center ${direction} ${className}`}>
-			{direction === "chat-start" && (
-				<Box className="avatar mr-5">
-					<Typography variant="h2">{emoticon}</Typography>
-				</Box>
-			)}
-			<Box className={`chat-bubble max-w-full bg-white text-black`}>{children}</Box>
-			{direction === "chat-end" && (
-				<Box className="avatar ml-5">
-					<Typography variant="h2">{emoticon}</Typography>
-				</Box>
-			)}
+			{renderAvatar}
+			<Box className="chat-bubble max-w-full bg-white text-black">{children}</Box>
+			{direction === "chat-end" && renderAvatar}
 		</Box>
 	);
 }
