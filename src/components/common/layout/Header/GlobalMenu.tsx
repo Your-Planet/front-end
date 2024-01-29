55a8d4ade34943965cb49df55799cd86026467ec
hyeonQyu <dhk0561@naver.com>
2024-01-30 00:14:08 +0900

    feat: AuthProvider를 통한 로그인 정보 주입

--- src/components/common/layout/Header/GlobalMenu.tsx
+++ src/components/common/layout/Header/GlobalMenu.tsx
@@ -1,12 +1,18 @@
+import { useAuthContext } from "@/providers/AuthProvider";
 import { Box } from "@mui/material";
 import { LABEL_BY_GLOBAL_MENU } from "./defines/constants";
 import { StyledHeaderLink } from "./defines/styles";
 
 function GlobalMenu() {
+	const { jwtPayload } = useAuthContext();
+
 	return (
 		<Box>
-			<StyledHeaderLink href="/login">{LABEL_BY_GLOBAL_MENU.LOG_IN}</StyledHeaderLink>
-			<StyledHeaderLink href="/">{LABEL_BY_GLOBAL_MENU.LOG_OUT}</StyledHeaderLink>
+			{jwtPayload ? (
+				<StyledHeaderLink href="/logout">{LABEL_BY_GLOBAL_MENU.LOG_OUT}</StyledHeaderLink>
+			) : (
+				<StyledHeaderLink href="/login">{LABEL_BY_GLOBAL_MENU.LOG_IN}</StyledHeaderLink>
+			)}
 			<StyledHeaderLink href="/register">{LABEL_BY_GLOBAL_MENU.REGISTER}</StyledHeaderLink>
 			<StyledHeaderLink href="/my-page">{LABEL_BY_GLOBAL_MENU.MY_PAGE}</StyledHeaderLink>
 		</Box>
