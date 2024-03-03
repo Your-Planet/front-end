c6883e40f6639a3217e13bc0961c2c7dc3ef1cf7
naeunchan <eunchan0492@naver.com>
2024-03-03 15:02:46 +0900

    로그인 시 이메일 기억하기 체크 되어 있으면  email 정보 쿠키에 저장
    Jira: YP-74

--- src/apis/member/request.ts
+++ src/apis/member/request.ts
@@ -1,7 +1,9 @@
 import { JoinRequest, JoinResponse } from "@/apis/member/models";
 import { LoginRequest, LoginResponse } from "@/apis/member/models/login";
+import { ACCOUNT_COOKIE } from "@/components/login/LoginView/defines/cookie/constants";
 import { AxiosRequestFunction } from "@/defines/apis/types";
 import BaseApi from "@/utils/api/BaseApi";
+import { removeCookie, setCookie } from "@/utils/cookie";
 
 export class MemberApi extends BaseApi {
 	public join: AxiosRequestFunction<JoinRequest, JoinResponse> = (req) => {
@@ -9,7 +11,13 @@ export class MemberApi extends BaseApi {
 	};
 
 	public login: AxiosRequestFunction<LoginRequest, LoginResponse> = (req) => {
-		const { email, password } = req;
+		const { email, password, isRemember } = req;
+
+		if (isRemember) {
+			setCookie(ACCOUNT_COOKIE.rememberUserEmail, email, ACCOUNT_COOKIE.maxAge);
+		} else {
+			removeCookie(ACCOUNT_COOKIE.rememberUserEmail);
+		}
 
 		return this.axiosInstance.post(this.getUrl("/login"), { email, password });
 	};
