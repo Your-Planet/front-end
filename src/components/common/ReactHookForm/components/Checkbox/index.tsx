07fd04f558c735eb1ad609b71c8de92ab04b8989
hyeonQyu <dhk0561@naver.com>
2024-03-06 20:18:41 +0900

    feat: 로그인 페이지 이메일 기억하기 훅폼으로 리팩토링

--- src/components/common/ReactHookForm/components/Checkbox/index.tsx
+++ src/components/common/ReactHookForm/components/Checkbox/index.tsx
@@ -1,45 +1,23 @@
 import { ReactHookFormProps } from "@/components/common/ReactHookForm/defines/types";
 import useReactHookFormControl from "@/components/common/ReactHookForm/hooks/useReactHookFormControl";
-import {
-	Checkbox,
-	CheckboxProps,
-	FormControl,
-	FormControlLabel,
-	FormGroup,
-	FormHelperText,
-	FormLabel,
-} from "@mui/material";
+import { Checkbox, CheckboxProps, FormControl, FormControlLabel, FormHelperText } from "@mui/material";
 import { FieldValues } from "react-hook-form";
 
 export interface ReactHookFormCheckboxProps<TFieldValues extends FieldValues, CheckboxValue extends string | number>
 	extends ReactHookFormProps<TFieldValues>,
-		Pick<CheckboxProps, "onChange"> {
-	label?: string;
-	checkbox: {
-		value: CheckboxValue;
-		checkboxLabel?: string;
-		checked?: boolean;
-	};
-	row?: boolean;
-}
+		Omit<CheckboxProps, "onChange" | "label" | "required"> {}
 
 function ReactHookFormCheckbox<TFieldValues extends FieldValues, CheckboxValue extends string | number>(
 	props: ReactHookFormCheckboxProps<TFieldValues, CheckboxValue>,
 ) {
-	const { formName, label, checkbox, row = false, onChange } = props;
-	const { restProps, field, error, errorMessage } = useReactHookFormControl(props);
-	const { value, checkboxLabel, checked } = checkbox;
+	const { restProps, field, label, error, errorMessage, handleChange } = useReactHookFormControl(props);
 
 	return (
 		<FormControl error={Boolean(error)}>
-			{label && <FormLabel id={formName}>{label}</FormLabel>}
-			<FormGroup row={row} {...field} aria-labelledby={formName}>
-				<FormControlLabel
-					key={value || checkboxLabel}
-					control={<Checkbox {...restProps} onChange={onChange} checked={checked} />}
-					label={checkboxLabel || value}
-				/>
-			</FormGroup>
+			<FormControlLabel
+				control={<Checkbox {...restProps} {...field} checked={field.value} onChange={handleChange} />}
+				label={label}
+			/>
 			{errorMessage && <FormHelperText>{errorMessage}</FormHelperText>}
 		</FormControl>
 	);
