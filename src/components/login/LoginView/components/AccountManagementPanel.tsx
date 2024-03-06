bedc7e221ad0aff81e1d68a3f50c502b7ebad4fc
naeunchan <eunchan0492@naver.com>
2024-03-06 22:27:43 +0900

    IA 객체의 label 사용
    Jira: YP-74

--- src/components/login/LoginView/components/AccountManagementPanel.tsx
+++ src/components/login/LoginView/components/AccountManagementPanel.tsx
@@ -7,15 +7,15 @@ function AccountManagementPanel() {
 	return (
 		<Box className="flex justify-center items-center text-center mt-8 gap-5">
 			<Link href={getIaPath(IA.find.email)} className="text-gray-500 no-underline">
-				이메일 찾기
+				{IA.find.email.label}
 			</Link>
 			<Divider orientation="vertical" flexItem />
 			<Link href={getIaPath(IA.find.pw)} className="text-gray-500 no-underline">
-				비밀번호 찾기
+				{IA.find.pw.label}
 			</Link>
 			<Divider orientation="vertical" flexItem />
 			<Link href={getIaPath(IA.join)} className="text-gray-500 no-underline">
-				계정 만들기
+				{IA.join.label}
 			</Link>
 		</Box>
 	);
