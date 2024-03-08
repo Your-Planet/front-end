d1d046672c4fb1a49b423ffcf4bfe706a92d8a73
hyeonQyu <dhk0561@naver.com>
2024-03-09 00:25:08 +0900

    fix: 빌드 에러 수정을 위한 NextResponse return 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
 import { COOKIE } from "@/defines/cookie/constants";
 import { getJwtPayload } from "@/utils/auth";
 import { getIaObject, getIsAccessiblePage } from "@/utils/ia";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (request: NextRequest) => {
 	const {
@@ -10,19 +9,23 @@ export const middleware = (request: NextRequest) => {
 		cookies,
 	} = request;
 
+	const response = NextResponse.next();
+
 	const jwtPayload = getJwtPayload(cookies.get(COOKIE.accessToken)?.value);
 
 	const currentIa = getIaObject(pathname);
 
 	if (!currentIa) {
 		console.error("IA not found for pathname", pathname);
-		return;
+		return response;
 	}
 
 	if (!getIsAccessiblePage(currentIa, jwtPayload)) {
 		// TODO @김현규 접근 권한 없음 에러 페이지로 변경
 		return NextResponse.redirect(new URL("/403", request.url));
 	}
+
+	return response;
 };
 
 export const config = {
