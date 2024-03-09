a88241a235e36339f318577e282aa4850dfb294c
hyeonQyu <dhk0561@naver.com>
2024-03-09 22:56:32 +0900

    refactor: AuthGuard 컴포넌트 제거

--- src/app/post-me/page.tsx
+++ src/app/post-me/page.tsx
@@ -1,4 +1,3 @@
-import AuthGuard from "@/components/common/AuthGuard";
 import InstagramIdField from "@/components/post_me/InstagramIdField";
 import InstatoonCategory from "@/components/post_me/InstatoonCategory";
 import IntroductionField from "@/components/post_me/IntroductionField";
@@ -10,15 +9,13 @@ type Props = {};
 
 function PostME(props: Props) {
 	return (
-		<AuthGuard allowedMemberTypes={["SPONSOR"]}>
-			<Box className="w-full flex flex-col justify-center items-center p-10">
-				<InstagramIdField />
-				<IntroductionField />
-				<InstatoonCategory />
-				<PortfolioLinkField />
-				<SubmitButton />
-			</Box>
-		</AuthGuard>
+		<Box className="w-full flex flex-col justify-center items-center p-10">
+			<InstagramIdField />
+			<IntroductionField />
+			<InstatoonCategory />
+			<PortfolioLinkField />
+			<SubmitButton />
+		</Box>
 	);
 }
 
