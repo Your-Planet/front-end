ec52df96504590dc32b0b6a2f5187ccc94b1f37d
hyeonQyu <dhk0561@naver.com>
2024-03-16 12:46:06 +0900

    feat: 인스타그램 엑세스 토큰 발급 서버 로직 추가

--- src/app/join/author/actions.ts
+++ src/app/join/author/actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { API } from "@/apis";
 import { DOMAIN } from "@/defines/domain/constants";
 import { objectToUrlParams } from "@/utils/object";
 import { getLeadingSlash } from "@/utils/string";
+
+const getRedirectUri = (redirectPath: string) => `${DOMAIN.yourPlanet}${getLeadingSlash(redirectPath)}`;
 
 export const getInstagramAuthUrl = async ({
 	redirectPath,
@@ -16,10 +19,27 @@ export const getInstagramAuthUrl = async ({
 
 	const params = objectToUrlParams({
 		client_id: process.env.INSTAGRAM_CLIENT_ID,
-		redirect_uri: `${DOMAIN.yourPlanet}${getLeadingSlash(redirectPath)}`,
+		redirect_uri: getRedirectUri(redirectPath),
 		scope: scopes.join(),
 		response_type: "code",
 	});
 
-	return `${DOMAIN.instagram.auth}/authorize?${params.toString()}`;
+	return `${DOMAIN.instagram.api}/oauth/authorize?${params.toString()}`;
+};
+
+export const getInstagramAccessToken = async ({ code, redirectPath }: { code: string; redirectPath: string }) => {
+	const { access_token, user_id } = (
+		await API.instagramAuth.accessToken({
+			client_id: process.env.INSTAGRAM_CLIENT_ID!,
+			client_secret: process.env.INSTAGRAM_SECRET_CODE!,
+			code,
+			grant_type: "authorization_code",
+			redirect_uri: getRedirectUri(redirectPath),
+		})
+	).data;
+
+	return {
+		accessToken: access_token,
+		userId: user_id,
+	};
 };
