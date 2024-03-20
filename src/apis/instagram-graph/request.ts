fda3c573f0fa97c657da61a9e62b4879c8edc461
hyeonQyu <dhk0561@naver.com>
2024-03-20 23:28:25 +0900

    feat: 인스타그램 me 조회 API 추가

--- src/apis/instagram-graph/request.ts
+++ src/apis/instagram-graph/request.ts
@@ -1,3 +1,4 @@
+import { MeRequest, MeResponse } from "@/apis/instagram-graph/models";
 import { LongLivedAccessTokenRequest, LongLivedAccessTokenResponse } from "@/apis/instagram-graph/models/accessToken";
 import { AxiosRequestFunction } from "@/defines/apis/types";
 import BaseApi from "@/utils/api/BaseApi";
@@ -8,6 +9,16 @@ export class InstagramGraphApi extends BaseApi {
 	) => {
 		return this.axiosInstance.get(this.getUrl("/access_token"), {
 			params: req,
+		});
+	};
+
+	public me: AxiosRequestFunction<MeRequest, MeResponse> = (req) => {
+		const { fields: fieldArray, ...rest } = req;
+		return this.axiosInstance.get(this.getUrl("/v19.0/me"), {
+			params: {
+				...rest,
+				fields: fieldArray?.join(","),
+			},
 		});
 	};
 }
