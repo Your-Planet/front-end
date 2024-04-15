3dc8ff8b1c56a0acefb506a562912d844478252c
hyeonQyu <dhk0561@naver.com>
2024-04-15 17:00:14 +0900

    feat: 인스타툰 카테고리 및 프로필 설정 form 타입 추가

---
+++ src/components/mypage/studio/StudioProfileView/defines/types.ts
@@ -0,0 +1,9 @@
+import { InstatoonCategoryType } from "@/defines/instatoon-category/types";
+
+// TODO @김현규 API request 객체 생성 후 상속하는 형태로 변경
+export interface StudioProfileForm {
+	name: string;
+	description: string;
+	category: Record<InstatoonCategoryType, boolean>;
+	portfolioLinks: string[];
+}
---
+++ src/defines/instatoon-category/types.ts
@@ -0,0 +1,15 @@
+export type InstatoonCategoryType =
+	| "EVERYDAY_HEALING"
+	| "ACTION"
+	| "HISTORICAL_DRAMA"
+	| "HUMOR"
+	| "THRILLER"
+	| "FASHION"
+	| "EMPATHY"
+	| "EPISODE"
+	| "ROMANCE"
+	| "SF"
+	| "HORROR"
+	| "SADNESS"
+	| "SPORTS"
+	| "TOUCHING_DRAMA";
