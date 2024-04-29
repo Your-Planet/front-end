a418a997c78465e3dea279406fa819911d447904
hyeonQyu <dhk0561@naver.com>
2024-04-30 07:26:39 +0900

    feat: 인스타그램 계정 정보 실패 시 에러 처리

--- src/components/mypage/studio/StudioProfileView/components/StudioProfileFormView/components/InstagramUserNameTextField.tsx
+++ src/components/mypage/studio/StudioProfileView/components/StudioProfileFormView/components/InstagramUserNameTextField.tsx
@@ -1,22 +1,49 @@
 "use client";
 
 import { AuthorJoinRequest } from "@/apis/member";
+import useOpen from "@/hooks/common/useOpen";
 import useQueryGetDetail from "@/hooks/queries/member/useQueryGetDetail";
-import { TextField } from "@mui/material";
+import { Alert, Snackbar, TextField } from "@mui/material";
+import { useEffect } from "react";
 
 export interface InstagramUserNameTextFieldProps {}
 
 function InstagramUserNameTextField(props: InstagramUserNameTextFieldProps) {
 	const {} = props;
 
-	const { data: { data: memberInfo } = {} } = useQueryGetDetail<AuthorJoinRequest>({
+	const { data: { data: memberInfo } = {}, isError } = useQueryGetDetail<AuthorJoinRequest>({
 		req: undefined,
 		queryOption: {
 			refetchOnWindowFocus: false,
 		},
 	});
 
-	return <TextField label="인스타그램 계정" disabled helperText=" " value={memberInfo?.instagramUsername} />;
+	const { opened: isErrorOpened, handleOpen: handleOpenError, handleClose: handleCloseError } = useOpen();
+
+	useEffect(() => {
+		if (isError) {
+			handleOpenError();
+		} else {
+			handleCloseError();
+		}
+	}, [isError]);
+
+	return (
+		<>
+			<Snackbar
+				open={isErrorOpened}
+				onClose={handleCloseError}
+				anchorOrigin={{ vertical: "top", horizontal: "right" }}
+				autoHideDuration={6000}
+			>
+				<Alert onClose={handleCloseError} severity="error" variant="filled" sx={{ width: "100%" }}>
+					인스타그램 계정 정보를 불러오는데 실패했습니다.
+				</Alert>
+			</Snackbar>
+
+			<TextField label="인스타그램 계정" disabled helperText=" " value={memberInfo?.instagramUsername} />
+		</>
+	);
 }
 
 export default InstagramUserNameTextField;
