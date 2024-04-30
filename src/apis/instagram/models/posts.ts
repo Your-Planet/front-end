c68599dfca6fd3842fc1a5e9b2e6a936c904a7b0
hyeonQyu <dhk0561@naver.com>
2024-04-30 15:59:32 +0900

    feat: 인스타그램 게시글 객체 타입 정의

--- src/apis/instagram/models/posts.ts
+++ src/apis/instagram/models/posts.ts
@@ -1,3 +1,19 @@
-export interface InstagramPostRequest {}
+export interface InstagramPost {
+	id: string;
+	caption: string;
+	is_shared_to_feed: boolean;
+	media_type: string;
+	media_url: string;
+	permalink: string;
+	thumbnail_url: string;
+	timestamp: string;
+	username: string;
+}
 
-export interface InstagramPostResponse {}
+export interface InstagramPostRequest {
+	permalink: string;
+}
+
+export interface InstagramPostResponse {
+	posts: InstagramPost[];
+}
