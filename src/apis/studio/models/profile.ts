00fc825fc1b142c825a71f1a3ce8cce250019d91
Hyeongyu Kim <dhk0561@naver.com>
2024-05-01 13:51:16 +0900

    feat: 작가 스튜디오 form 타입 수정

--- src/apis/studio/models/profile.ts
+++ src/apis/studio/models/profile.ts
@@ -1,7 +1,10 @@
 import { InstatoonCategoryType } from "@/defines/instatoon-category/types";
 
 interface StudioProfile {
+	name: string;
+	description: string;
 	categories: InstatoonCategoryType[];
+	portfolioIds: number[];
 }
 
 export interface PostProfileRequest extends StudioProfile {}
