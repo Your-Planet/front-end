cee4a2bd6c6e50e36aa2b35a79855758eeb846c8
hyeonQyu <dhk0561@naver.com>
2024-05-01 23:06:43 +0900

    feat: 포트폴리오 링크로 포트폴리오 조회 API 연동

--- src/components/mypage/studio/StudioProfileView/defines/types.ts
+++ src/components/mypage/studio/StudioProfileView/defines/types.ts
@@ -1,11 +1,8 @@
+import { InstagramPost } from "@/apis/instagram";
 import { PostProfileRequest } from "@/apis/studio";
 import { InstatoonCategoryType } from "@/defines/instatoon-category/types";
 
-// TODO @김현규 인스타그램 게시글 타입 Pick 하는 것으로 변경
-export type Portfolio = {
-	id: number;
-	permalink: string;
-};
+export type Portfolio = Pick<InstagramPost, "id" | "permalink">;
 
 export interface StudioProfileForm extends Pick<PostProfileRequest, "name" | "description"> {
 	name: string;
--- src/hooks/queries/instagram/useQueryGetPosts.ts
+++ src/hooks/queries/instagram/useQueryGetPosts.ts
@@ -3,7 +3,9 @@ import { InstagramPostRequest, InstagramPostResponse } from "@/apis/instagram";
 import { ResponseEntity } from "@/defines/apis/types";
 import { QUERY_KEY } from "@/defines/react-query/constants";
 import { UseQueryParams } from "@/defines/react-query/types";
-import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { TIME_UNIT } from "@/defines/time/constants";
+import { fetchQuery } from "@/utils/react-query";
+import { UseQueryResult, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
 type Request = InstagramPostRequest;
@@ -24,6 +26,17 @@ function useQueryGetPosts(params: UseQueryGetPostsParams): UseQueryGetPosts {
 		queryFn: async () => (await API.instagram.getPosts(req)).data,
 		...queryOption,
 	});
+}
+
+export function useFetchQueryGetPosts() {
+	const queryClient = useQueryClient();
+
+	return (req: Request) =>
+		fetchQuery<Response>(queryClient, {
+			queryKey: QUERY_KEY.instagram.posts(req),
+			queryFn: async () => (await API.instagram.getPosts(req)).data,
+			staleTime: 3 * TIME_UNIT.unitOfMs.asMinute,
+		});
 }
 
 export default useQueryGetPosts;
