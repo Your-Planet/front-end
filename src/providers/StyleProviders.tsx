576f1bbd22f1a65572d40ba04ee4a4987897e4d6
naeunchan <eunchan0492@naver.com>
2024-05-11 17:05:53 +0900

    Font 수정

--- src/providers/StyleProviders.tsx
+++ src/providers/StyleProviders.tsx
@@ -2,15 +2,13 @@
 
 // To use tailwind and mui together
 import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider } from "@mui/material";
-import "pretendard/dist/web/variable/pretendardvariable.css";
 import { ReactNode } from "react";
 
 const StyleProviders = ({ children }: { children: ReactNode }) => {
 	const theme = createTheme({
 		typography: {
 			fontFamily: [
-				"Pretendard Variable",
-				"Pretendard",
+				"Futura",
 				"-apple-system",
 				"BlinkMacSystemFont",
 				"system-ui",
