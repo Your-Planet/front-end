fe87a68b1eaf755789efa862dc01385d4939eb12
Hyeongyu Kim <dhk0561@naver.com>
2024-05-19 17:25:16 +0900

    [YP-306] feat: 미리보기 컴포넌트 분리 및 레이아웃 변경

--- src/components/mypage/studio/defines/constants.ts
+++ src/components/mypage/studio/defines/constants.ts
@@ -1,5 +1,7 @@
+import { AUTHOR_CARD_WIDTH } from "@/components/common/AuthorCard";
+
 export const STUDIO_FORM_WIDTH = 520;
 
-export const STUDIO_PROFILE_PREVIEW_WIDTH = 500;
+export const STUDIO_PROFILE_PREVIEW_WIDTH = AUTHOR_CARD_WIDTH;
 
 export const STUDIO_PROFILE_GAP = 48;
