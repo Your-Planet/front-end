5171dac30ae588ffb25e66ad941983953f759418
Hyeongyu Kim <dhk0561@naver.com>
2024-06-16 01:19:13 +0900

    [YP-311] feat: handleRequestRejected 추가

--- src/providers/AxiosConfigRegistry/hooks/useInternalAxiosConfig/index.ts
+++ src/providers/AxiosConfigRegistry/hooks/useInternalAxiosConfig/index.ts
@@ -4,13 +4,14 @@ import { AxiosInstance } from "axios";
 import { useEffect } from "react";
 
 export default function useInternalAxiosConfig(axiosInstances: AxiosInstance[]) {
-	const { handleFulfilled: handleRequestFulfilled } = useInternalAxiosRequestInterceptor();
+	const { handleFulfilled: handleRequestFulfilled, handleRejected: handleRequestRejected } =
+		useInternalAxiosRequestInterceptor();
 	const { handleFulfilled: handleResponseFulfilled, handleRejected: handleResponseRejected } =
 		useInternalAxiosResponseInterceptor();
 
 	useEffect(() => {
 		axiosInstances.forEach((axiosInstance) => {
-			axiosInstance.interceptors.request.use(handleRequestFulfilled);
+			axiosInstance.interceptors.request.use(handleRequestFulfilled, handleRequestRejected);
 			axiosInstance.interceptors.response.use(handleResponseFulfilled, handleResponseRejected);
 		});
 	}, [axiosInstances]);
