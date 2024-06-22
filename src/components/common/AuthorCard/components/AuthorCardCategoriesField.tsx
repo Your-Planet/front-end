88143d3c07ce97fc1e6e9d899bdc469433b31633
Hyeongyu Kim <dhk0561@naver.com>
2024-06-22 22:30:37 +0900

    [YP-353] fix: 빌드 에러 수정

--- src/components/common/AuthorCard/components/AuthorCardCategoriesField.tsx
+++ src/components/common/AuthorCard/components/AuthorCardCategoriesField.tsx
@@ -17,7 +17,7 @@ function AuthorCardCategoriesField(props: AuthorCardCategoriesFieldProps) {
 				? Array.from({ length: 3 }).map((_, i) => (
 						<Skeleton key={i} variant="rounded" sx={{ borderRadius: 10 }} width={38} height={24} />
 					))
-				: value.map((category) => <CategoryChip categoryType={category} />)}
+				: value.map((category) => <CategoryChip key={category} categoryType={category} />)}
 		</Stack>
 	);
 }
