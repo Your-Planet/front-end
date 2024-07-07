e0dde3955dedf347f2ff58366a836f52bb48fd48
naeunchan <eunchan0492@naver.com>
2024-07-07 22:26:58 +0900

    Global Menu 로그인/로그아웃 bug fix

--- src/components/common/layout/Header/components/GlobalMenu/components/GlobalMenuLink/index.tsx
+++ src/components/common/layout/Header/components/GlobalMenu/components/GlobalMenuLink/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { StyledHeaderLink } from "@/components/common/layout/Header/components/GlobalMenu/components/GlobalMenuLink/defines/styles";
 import { PageAttributes } from "@/defines/ia/types";
 import { getIaPath } from "@/utils/ia";
