8bd079b683226958e725d16b28b8d0e8d3b1e6e9
Hyeongyu Kim <dhk0561@naver.com>
2024-07-27 23:12:35 +0900

    [YP-415] feat: 프로필 이미지 필수값 설정

--- src/components/mypage/studio/StudioProfileView/index.tsx
+++ src/components/mypage/studio/StudioProfileView/index.tsx
@@ -2,29 +2,12 @@
 
 import StudioProfileFormView from "@/components/mypage/studio/StudioProfileView/components/StudioProfileFormView";
 import { StudioProfilePreview } from "@/components/mypage/studio/StudioProfileView/components/StudioProfilePreview";
-import { DEFAULT_CATEGORY, DEFAULT_PORTFOLIO } from "@/components/mypage/studio/StudioProfileView/defines/constants";
-import { StudioProfileForm } from "@/components/mypage/studio/StudioProfileView/defines/types";
+import StudioProfileFormProvider from "@/components/mypage/studio/StudioProfileView/provider/StudioProfileFormProvider";
 import { STUDIO_PROFILE_GAP, STUDIO_VIEW_PADDING } from "@/components/mypage/studio/defines/constants";
 import MemberDetailProvider from "@/providers/MemberDetailProvider";
 import { Box } from "@mui/material";
-import { FormProvider, useForm } from "react-hook-form";
 
-interface StudioProfileViewProps {}
-
-function StudioProfileView(props: StudioProfileViewProps) {
-	const {} = props;
-
-	const form = useForm<StudioProfileForm>({
-		mode: "all",
-		defaultValues: {
-			name: "",
-			description: "",
-			category: DEFAULT_CATEGORY,
-			portfolios: [DEFAULT_PORTFOLIO],
-			profileImage: null,
-		},
-	});
-
+function StudioProfileView() {
 	return (
 		<Box
 			sx={{
@@ -34,10 +17,10 @@ function StudioProfileView(props: StudioProfileViewProps) {
 			}}
 		>
 			<MemberDetailProvider>
-				<FormProvider {...form}>
+				<StudioProfileFormProvider>
 					<StudioProfileFormView />
 					<StudioProfilePreview />
-				</FormProvider>
+				</StudioProfileFormProvider>
 			</MemberDetailProvider>
 		</Box>
 	);
