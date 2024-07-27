5e8dd4017c9f5213b079cff52b33c794ee87e320
Hyeongyu Kim <dhk0561@naver.com>
2024-07-28 00:28:35 +0900

    [YP-416] chore: 개발 환경 백엔드 서버 URL 변경

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const EC2_SERVER_URL = "http://ec2-15-164-160-23.ap-northeast-2.compute.amazonaws.com:8080";
+
 const SERVER_URL_BY_ENVIRONMENT = {
-	development: "http://localhost:8080",
+	development: EC2_SERVER_URL,
 	test: "http://localhost:8080",
-	production: "http://ec2-15-164-160-23.ap-northeast-2.compute.amazonaws.com:8080",
+	production: EC2_SERVER_URL,
 };
 
 const nextConfig = {
