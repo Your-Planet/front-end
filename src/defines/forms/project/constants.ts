5a78be2907854202dfe0a4a4570eaa8b1d6eb621
Hyeongyu Kim <dhk0561@naver.com>
2024-08-24 14:19:25 +0900

    [YP-507] feat: 프로젝트 의뢰 관련 타입 및 상수 정의

---
+++ src/defines/forms/project/constants.ts
@@ -0,0 +1,25 @@
+export const PROJECT_FORM_LENGTH = {
+	campaignDescription: {
+		min: 1,
+		max: 500,
+	},
+	referenceUrls: {
+		min: 0,
+		max: 5,
+	},
+	referenceFiles: {
+		min: 0,
+		max: 10,
+	},
+	message: {
+		min: 0,
+		max: 1000,
+	},
+};
+
+export const PROJECT_FORM_SIZE = {
+	referenceFiles: {
+		min: 0,
+		max: 20, // MB
+	},
+};
