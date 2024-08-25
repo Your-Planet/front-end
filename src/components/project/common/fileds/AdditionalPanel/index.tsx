8539cd4a22a2f31601ff74cb003bf86841489d18
naeunchan <eunchan0492@naver.com>
2024-08-26 00:31:39 +0900

    select -> textfield select로 수정

--- src/components/project/common/fileds/AdditionalPanel/index.tsx
+++ src/components/project/common/fileds/AdditionalPanel/index.tsx
@@ -1,41 +1,53 @@
 import ReactHookForm from "@/components/common/ReactHookForm";
 import { ProjectCommonForm, ProjectFormFieldCommonProps } from "@/defines/forms/project/types";
-import { Box, FormHelperText, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
+import { Box, MenuItem } from "@mui/material";
 import { useFormContext } from "react-hook-form";
 
 function AdditionalPanel(props: ProjectFormFieldCommonProps) {
-	const { formName, helperText } = props;
+	const { formName, helperText, required } = props;
 
 	const { watch, setValue } = useFormContext<ProjectCommonForm>();
 
-	const { Checkbox } = ReactHookForm<ProjectCommonForm>();
-
-	const handleChangeSelect = (event: SelectChangeEvent) => {
-		setValue("additionalPanel.count", Number(event.target.value));
-	};
+	const { TextField, Checkbox } = ReactHookForm<ProjectCommonForm>();
 
 	const [count, isNegotiable] = watch(["additionalPanel.count", "additionalPanel.isNegotiable"]);
 
+	const getAdditionalPanelCountItems = () => {
+		// TODO: 나은찬 작가 기본 제공 컷수 파라미터 받아서 처리
+		const tempCount = 10;
+
+		return Array.from({ length: tempCount }, (_, i) => {
+			if (i) {
+				return (
+					<MenuItem key={i} value={i}>
+						{i}장
+					</MenuItem>
+				);
+			} else {
+				return (
+					<MenuItem key={i} value={i}>
+						추가 안함
+					</MenuItem>
+				);
+			}
+		});
+	};
+
 	return (
 		<Box>
-			<Typography color="grey" fontWeight="bold">
-				추가 컷 수
-			</Typography>
-			<FormHelperText>{helperText}</FormHelperText>
 			<Box>
-				<Select disabled={isNegotiable} onChange={handleChangeSelect} value={count.toString()} fullWidth>
-					{/* TODO: 나은찬, 작가 별 기본 컷수 받아서 처리 */}
-					<MenuItem value={0}>추가 안함</MenuItem>
-					<MenuItem value={1}>1장</MenuItem>
-					<MenuItem value={2}>2장</MenuItem>
-					<MenuItem value={3}>3장</MenuItem>
-					<MenuItem value={4}>4장</MenuItem>
-					<MenuItem value={5}>5장</MenuItem>
-					<MenuItem value={6}>6장</MenuItem>
-					<MenuItem value={7}>7장</MenuItem>
-					<MenuItem value={8}>8장</MenuItem>
-					<MenuItem value={9}>9장</MenuItem>
-				</Select>
+				<TextField
+					select
+					fullWidth
+					label="추가 컷 수"
+					formName={formName}
+					disabled={isNegotiable}
+					value={count.toString()}
+					required={required}
+					helperText={helperText}
+				>
+					{getAdditionalPanelCountItems()}
+				</TextField>
 				<Checkbox formName="additionalPanel.isNegotiable" label={"작가와 협의 할래요"} hideErrorMessage />
 			</Box>
 		</Box>
