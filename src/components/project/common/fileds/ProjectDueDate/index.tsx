949272ad5bce73a85be4b24eaef2ce7ef606e8f6
Hyeongyu Kim <dhk0561@naver.com>
2024-08-25 21:01:12 +0900

    [YP-485] feat: 작업 기한 오늘 이전 날짜 선택 불가 처리

--- src/components/project/common/fileds/ProjectDueDate/index.tsx
+++ src/components/project/common/fileds/ProjectDueDate/index.tsx
@@ -1,6 +1,5 @@
 import ReactHookForm from "@/components/common/ReactHookForm";
 import { ProjectCommonForm, ProjectFormFieldCommonProps } from "@/defines/forms/project/types";
-import dayjs, { Dayjs } from "dayjs";
 
 export interface ProjectDueDateProps extends ProjectFormFieldCommonProps {}
 
@@ -9,27 +8,18 @@ function ProjectDueDate(props: ProjectDueDateProps) {
 
 	const { DatePicker } = ReactHookForm<ProjectCommonForm>();
 
-	const validate = (value: Dayjs) => {
-		if (!value.isAfter(dayjs(), "day")) {
-			return "오늘 이후의 날짜를 선택해주세요.";
-		}
-		return true;
-	};
-
 	return (
 		<DatePicker
 			formName={formName}
 			label="작업 기한"
 			required={required}
 			helperText={helperText}
-			rules={{
-				validate,
-			}}
 			slotProps={{
 				textField: {
 					fullWidth: true,
 				},
 			}}
+			disablePast
 		/>
 	);
 }
