56e80dd9bb249eba74e92b7828b742b41cb65497
naeunchan <eunchan0492@naver.com>
2024-09-01 19:03:28 +0900

    useWatch

--- src/components/project/common/fileds/AdditionalModification/index.tsx
+++ src/components/project/common/fileds/AdditionalModification/index.tsx
@@ -1,37 +1,29 @@
 import ReactHookForm from "@/components/common/ReactHookForm";
 import { ProjectCommonForm, ProjectFormFieldCommonProps } from "@/defines/forms/project/types";
 import { Box, MenuItem } from "@mui/material";
-import { useFormContext } from "react-hook-form";
+import { useWatch } from "react-hook-form";
 
 function AdditionalModification(props: ProjectFormFieldCommonProps) {
 	const { formName, helperText, required } = props;
 
-	const { watch } = useFormContext<ProjectCommonForm>();
+	const { TextField } = ReactHookForm<ProjectCommonForm>();
 
-	const { TextField, Checkbox } = ReactHookForm<ProjectCommonForm>();
+	const count = useWatch<ProjectCommonForm>({
+		name: "additionalModification.count",
+	});
 
 	const getAdditionalModificationCount = () => {
 		// TODO: 나은찬 작가 기본 수정 횟수 파라미터 받아서 처리
 		const tempCount = 10;
 
 		return Array.from({ length: tempCount }, (_, i) => {
-			if (i === 0) {
-				return (
-					<MenuItem key={i} value={i}>
-						추가 안함
-					</MenuItem>
-				);
-			}
-
 			return (
 				<MenuItem key={i} value={i}>
-					{i}장
+					{i === 0 ? "추가 안함" : `${i}회`}
 				</MenuItem>
 			);
 		});
 	};
-
-	const count = watch("additionalModification.count");
 
 	return (
 		<Box>
@@ -40,8 +32,8 @@ function AdditionalModification(props: ProjectFormFieldCommonProps) {
 					select
 					fullWidth
 					label="추가 수정 횟수"
-					formName={formName}
-					value={count.toString()}
+					formName="additionalModification.count"
+					defaultValue={count}
 					required={required}
 					helperText={helperText}
 				>
