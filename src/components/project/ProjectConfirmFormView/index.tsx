253fe32ff6598fe1521c7f87f279179dba3868f9
naeunchan <eunchan0492@naver.com>
2024-09-20 22:29:48 +0900

    프로젝트 요약 페이지 및 광고주명 필드 추가

---
+++ src/components/project/ProjectConfirmFormView/index.tsx
@@ -0,0 +1,22 @@
+import ProjectTextField from "@/components/project/common/fileds/ProjectTextField";
+import { PROJECT_FORM_WIDTH } from "@/defines/forms/project/constants";
+import { Box } from "@mui/material";
+
+function ProjectConfirmFormView() {
+	// TODO: @나은찬 수정 필요
+	return (
+		<Box
+			sx={{
+				width: `${PROJECT_FORM_WIDTH}px`,
+				display: "flex",
+				flexDirection: "column",
+				gap: "32px",
+				paddingY: "2rem",
+			}}
+		>
+			<ProjectTextField label="광고주명" text="마이 플래닛" />
+		</Box>
+	);
+}
+
+export default ProjectConfirmFormView;
