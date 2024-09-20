6e41cb2c7993bc7d260ced7fe32f4103732794b0
naeunchan <eunchan0492@naver.com>
2024-09-20 22:53:31 +0900

    textfield 수정

--- src/components/project/common/fileds/ProjectTextField/index.tsx
+++ src/components/project/common/fileds/ProjectTextField/index.tsx
@@ -2,13 +2,17 @@ import { TextField } from "@mui/material";
 
 type Props = {
 	label: string;
-	text: string;
+	text?: string;
+	defaultCuts?: number;
+	additionalCuts?: number;
 };
 
 function ProjectTextField(props: Props) {
-	const { label, text } = props;
+	const { label, text, defaultCuts = 0, additionalCuts = 0 } = props;
+	const defaultValue =
+		text ?? `총 ${defaultCuts + additionalCuts}컷 (기본 ${defaultCuts}컷 + 추가 ${additionalCuts}컷)`;
 
-	return <TextField label={label} defaultValue={text} InputProps={{ readOnly: true }} />;
+	return <TextField label={label} defaultValue={defaultValue} InputProps={{ readOnly: true }} />;
 }
 
 export default ProjectTextField;
