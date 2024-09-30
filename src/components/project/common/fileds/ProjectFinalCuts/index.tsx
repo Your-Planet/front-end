dd94b31e160e9851bd21e486b66e9f71ff5d79cb
naeunchan <eunchan0492@naver.com>
2024-09-30 22:38:28 +0900

    set value using useFormContext

--- src/components/project/common/fileds/ProjectFinalCuts/index.tsx
+++ src/components/project/common/fileds/ProjectFinalCuts/index.tsx
@@ -7,14 +7,16 @@ export interface ProjectFinalCutsProps extends ProjectFormFieldCommonProps {}
 function ProjectFinalCuts(props: ProjectFinalCutsProps) {
 	const { formName } = props;
 	const { TextField } = ReactHookForm<ProjectCommonForm>();
-	const { getValues } = useFormContext<ProjectCommonForm>();
+	const { getValues, setValue } = useFormContext<ProjectCommonForm>();
 	// TODO: @나은찬 default cuts 값 로드
 	const defaultCuts = 1;
 	const additionalCuts = getValues("additionalPanel.count");
 	const finalCuts = defaultCuts + additionalCuts;
 	const value = `총 ${finalCuts}컷 (기본 ${defaultCuts}컷 + 추가 ${additionalCuts}컷)`;
 
-	return <TextField formName={formName} label="최종 컷 수" value={value} InputProps={{ readOnly: true }} />;
+	setValue(formName, value);
+
+	return <TextField formName={formName} label="최종 컷 수" InputProps={{ readOnly: true }} />;
 }
 
 export default ProjectFinalCuts;
