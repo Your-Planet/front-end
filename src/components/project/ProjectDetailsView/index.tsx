178d42c00864dc852a17fe5e14589d698c872067
naeunchan <eunchan0492@naver.com>
2024-10-12 16:00:27 +0900

    chip style 수정

--- src/components/project/ProjectDetailsView/index.tsx
+++ src/components/project/ProjectDetailsView/index.tsx
@@ -1,17 +1,11 @@
 import NegotiationHistoryTab from "@/components/project/components/NegotiationHistoryTab/NegotiationHistoryTabs";
 import { Box } from "@mui/material";
-import NegotiationHistoryChip from "../components/NegotiationHistoryChip/index";
 
 function ProjectDetailsView() {
 	return (
 		<Box>
 			{/* TODO: @나은찬 pass the prop */}
 			<NegotiationHistoryTab currentNegotiationProgress="IN_NEGOTIATION" />
-			<NegotiationHistoryChip label="회신 필요" color="error" />
-			<NegotiationHistoryChip label="진행 중" disabled />
-			<NegotiationHistoryChip label="발송 완료" color="primary" />
-			<NegotiationHistoryChip label="발송 지연" color="error" />
-			<NegotiationHistoryChip label="수정 요청" color="success" />
 		</Box>
 	);
 }
--- src/components/project/components/NegotiationHistoryChip/index.tsx
+++ src/components/project/components/NegotiationHistoryChip/index.tsx
@@ -1,3 +1,7 @@
+import {
+	PROJECT_TAB_HEIGHT as PROJECT_CHIP_HEIGHT,
+	PROJECT_TAB_WIDTH as PROJECT_CHIP_WIDTH,
+} from "@/defines/forms/project/constants";
 import { Chip, ChipOwnProps } from "@mui/material";
 
 interface NegotiationChipProps extends ChipOwnProps {}
@@ -6,7 +10,13 @@ function NegotiationHistoryChip(props: NegotiationChipProps) {
 	const { disabled = false, color, label } = props;
 
 	return (
-		<Chip label={label} disabled={disabled} color={color} size="small" sx={{ fontWeight: "bold", width: "70px" }} />
+		<Chip
+			label={label}
+			disabled={disabled}
+			color={color}
+			size="small"
+			sx={{ fontWeight: "bold", width: `${PROJECT_CHIP_WIDTH}px`, height: `${PROJECT_CHIP_HEIGHT}px` }}
+		/>
 	);
 }
 
